8b06be7aeca274a589bf7b8d1348d02b51a91922
Sandmeert <satanderm@gmail.com>
2016-04-30 11:23:24 +0200

    Fix too many files

--- countries.csv.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var countries = JSON.parse(fs.readFileSync("result.json"));
-var csv = "COUNTRY, VALUE";
-for (var country in countries) {
-	if (countries.hasOwnProperty(country)) {
-		csv += "\n" + country + "," + countries[country].eu;
-	}
-}
-fs.writeFileSync('results/countries.csv', csv);
