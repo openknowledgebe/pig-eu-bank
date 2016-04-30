3ce0454f2746b11468300e18464aca0edc8e8eed
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2016-04-30 12:42:19 +0200

    Add extra level of detail (funding) under categories

--- data/convert.js
+++ data/convert.js
@@ -10,20 +10,27 @@ function convert(obj) {
 	obj.data.forEach(function(entry) {
 		var code = entry[8].trim();
 		var country = entry[9].trim();
+		var funding = entry[13].trim();
 		var category = entry[16].trim();
 		var eu = entry[20].trim();
 		var nat = entry[21].trim();
 		var total = entry[22].trim();
 		if (!countries[country]) {
-			countries[country] = { code : code, categories : {}, eu : 0};
+			countries[country] = { code : code, categories : {}, eu : 0 };
 		}
 		if (!countries[country].categories[category]) {
-			countries[country].categories[category] = { eu : 0, nat : 0, total : 0};
+			countries[country].categories[category] = { fundings : {}, eu: 0, nat: 0, total: 0 };
+		}
+		if (!countries[country].categories[category].fundings[funding]) {
+			countries[country].categories[category].fundings[funding] = { eu : 0, nat : 0, total : 0 };
 		}
 		countries[country].eu += Number(eu);
 		countries[country].categories[category].eu += Number(eu);
 		countries[country].categories[category].nat += Number(nat);
 		countries[country].categories[category].total += Number(total);
+		countries[country].categories[category].fundings[funding].eu += Number(eu);
+		countries[country].categories[category].fundings[funding].nat += Number(nat);
+		countries[country].categories[category].fundings[funding].total += Number(total);
 	});
 	return countries;
 }
@@ -42,14 +49,20 @@ function addPopulation(countries) {
 }
 
 function changeToCapita(countries) {
-	Object.keys(countries).forEach(function(key) {
-		var country = countries[key];
+	Object.keys(countries).forEach(function(countryKey) {
+		var country = countries[countryKey];
 		country.eu /= country.population;
-		Object.keys(country.categories).forEach(function(key) {
-			var category = country.categories[key];
+		Object.keys(country.categories).forEach(function(categoryKey) {
+			var category = country.categories[categoryKey];
 			category.eu /= country.population;
 			category.nat /= country.population;
 			category.total /= country.population;
+			Object.keys(category.fundings).forEach(function(fundingKey) {
+				var funding = category.fundings[fundingKey];
+				funding.eu /= country.population;
+				funding.nat /= country.population;
+				funding.total /= country.population;
+			});
 		});
 	});
 }
