710931900ed03f7ea0f410f2746f8e6f8f31367e
Sandmeert <satanderm@gmail.com>
2016-04-30 11:24:46 +0200

    Last time failing folder restructure

---
+++ data/categories.separate.csv.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var countries = JSON.parse(fs.readFileSync("result.json"));
+Object.keys(countries).forEach(function(key) {
+	var country = countries[key];
+	var csv = "CATEGORY, VALUE";
+	Object.keys(country.categories).forEach(function(key) {
+		var category = country.categories[key];
+		csv += "\n" + key.replace(/\n/g, "") + ", " + category.eu;
+	});
+	fs.writeFileSync('results/countries/' + key + '.csv', csv);
+});
---
+++ data/convert.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var result = convert(JSON.parse(fs.readFileSync("datasources/rows.json")));
+var population = JSON.parse(fs.readFileSync("datasources/population.json"));
+addPopulation(result);
+changeToCapita(result);
+fs.writeFileSync('result.json', JSON.stringify(result));
+
+function convert(obj) {
+	var countries = {};
+	obj.data.forEach(function(entry) {
+		var code = entry[8].trim();
+		var country = entry[9].trim();
+		var category = entry[16].trim();
+		var eu = entry[20].trim();
+		var nat = entry[21].trim();
+		var total = entry[22].trim();
+		if (!countries[country]) {
+			countries[country] = { code : code, categories : {}, eu : 0};
+		}
+		if (!countries[country].categories[category]) {
+			countries[country].categories[category] = { eu : 0, nat : 0, total : 0};
+		}
+		countries[country].eu += Number(eu);
+		countries[country].categories[category].eu += Number(eu);
+		countries[country].categories[category].nat += Number(nat);
+		countries[country].categories[category].total += Number(total);
+	});
+	return countries;
+}
+
+function addPopulation(countries) {
+	for (var country in countries) {
+		if (countries.hasOwnProperty(country)) {
+			var pop = population.find(function(pop) {
+				return pop.GEO === country && pop.TIME === 2015;
+			});
+			if (pop) {
+				countries[country].population = Number(pop.Value.replace(/,/g,""));
+			}
+		}
+	}
+}
+
+function changeToCapita(countries) {
+	Object.keys(countries).forEach(function(key) {
+		var country = countries[key];
+		country.eu /= country.population;
+		Object.keys(country.categories).forEach(function(key) {
+			var category = country.categories[key];
+			category.eu /= country.population;
+			category.nat /= country.population;
+			category.total /= country.population;
+		});
+	});
+}
